effcb148ff12d58cb63b5260ef223caf68e25a63
williamcsa04 <williamcaetano14@gmail.com>
2022-06-29 22:34:30 -0300

    :sparkles: Created logic to save users password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,18 @@ import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './users.schema';
+import { scryptSync, randomBytes, timingSafeEqual } from 'crypto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   create(createUserDto: CreateUserDto): Promise<User> {
+    const salt = randomBytes(16).toString('hex');
+    createUserDto.password = `${salt}:${scryptSync(
+      createUserDto.password,
+      salt,
+      64,
+    ).toString('hex')}`;
     const newUser = new this.userModel(createUserDto);
     return newUser.save();
   }
@@ -18,6 +25,11 @@ export class UsersService {
   }
 
   findOne(id: number) {
+    // LOGIN CODE
+    // const [salt, key] = user.password.split(':');
+    // const hashedBuffer = scryptSync(user.password, salt, 64);
+    // const buffer = Buffer.from(key, 'hex');
+    // const match = timingSafeEqual(hashedBuffer, buffer)
     return `This action returns a #${id} user`;
   }
 
