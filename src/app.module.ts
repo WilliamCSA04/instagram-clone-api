085a636ebeb2ce07b4bd4914b42ac001249e5c13
williamcsa04 <williamcaetano14@gmail.com>
2022-06-29 22:13:11 -0300

    :poop: Setup basic insert

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
