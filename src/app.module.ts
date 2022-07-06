d59eb172ea4ccb57fc1b57c56c9c4cc3d228adc9
williamcsa04 <williamcaetano14@gmail.com>
2022-07-05 22:56:12 -0300

    :construction: Created post crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     AuthModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
